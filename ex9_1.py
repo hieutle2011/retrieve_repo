c9b03f34ba21d4ecca9e22724f675c4ea3f91bcd
hieule <hieuletrung102@gmail.com>
2017-10-02 22:40:28 +0700

    add

---
+++ ex9_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+import requests
+
+
+def githubrepos(username):
+    link = 'https://api.github.com/users/{0}/repos'
+    res = requests.get(link.format(username))
+    repos = res.json()
+    try:
+        result = [repo['name'] for repo in repos]
+        return result
+    except TypeError:
+        print('There is no such existing username')
+
+
+def main():
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+        print(githubrepos(username))
+    else:
+        print('No username provided')
+
+
+if __name__ == "__main__":
+    main()
